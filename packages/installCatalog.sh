b814c181ba222bb32fccffcb5075ab0069d1240b
Jason Peterson <jasonpet@us.ibm.com>
2018-10-29 23:23:52 -0400

    fix installCatalog

--- packages/installCatalog.sh
+++ packages/installCatalog.sh
@@ -44,96 +44,96 @@ $WSK_CLI -i --apihost "$APIHOST" package update --auth "$AUTH" --shared yes clou
 # Cloudant account actions
 
 $WSK_CLI -i --apihost "$APIHOST" action update --kind "$ACTION_RUNTIME_VERSION" --auth "$AUTH" cloudant/create-database \
-    "$PACKAGE_HOME/packages/account-actions/create-database.js" \
+    "$PACKAGE_HOME/account-actions/create-database.js" \
     -a description 'Create Cloudant database' \
     -a parameters '[ {"name":"dbname", "required":true} ]'
 
 $WSK_CLI -i --apihost "$APIHOST" action update --kind "$ACTION_RUNTIME_VERSION" --auth "$AUTH" cloudant/read-database \
-    "$PACKAGE_HOME/packages/account-actions/read-database.js" \
+    "$PACKAGE_HOME/account-actions/read-database.js" \
     -a description 'Read Cloudant database' \
     -a parameters '[ {"name":"dbname", "required":true} ]'
 
 $WSK_CLI -i --apihost "$APIHOST" action update --kind "$ACTION_RUNTIME_VERSION" --auth "$AUTH" cloudant/delete-database \
-    "$PACKAGE_HOME/packages/account-actions/delete-database.js" \
+    "$PACKAGE_HOME/account-actions/delete-database.js" \
     -a description 'Delete Cloudant database' \
     -a parameters '[ {"name":"dbname", "required":true} ]'
 
 $WSK_CLI -i --apihost "$APIHOST" action update --kind "$ACTION_RUNTIME_VERSION" --auth "$AUTH" cloudant/list-all-databases \
-    "$PACKAGE_HOME/packages/account-actions/list-all-databases.js" \
+    "$PACKAGE_HOME/account-actions/list-all-databases.js" \
     -a description 'List all Cloudant databases'
 
 $WSK_CLI -i --apihost "$APIHOST" action update --kind "$ACTION_RUNTIME_VERSION" --auth "$AUTH" cloudant/read-updates-feed \
-    "$PACKAGE_HOME/packages/account-actions/read-updates-feed.js" \
+    "$PACKAGE_HOME/account-actions/read-updates-feed.js" \
     -a description 'Read updates feed from Cloudant account (non-continuous)' \
     -a parameters '[ {"name":"dbname", "required":true}, {"name":"params", "required":false} ]'
 
 # Cloudant database actions
 
 $WSK_CLI -i --apihost "$APIHOST" action update --kind "$ACTION_RUNTIME_VERSION" --auth "$AUTH" cloudant/create-document \
-    "$PACKAGE_HOME/packages/database-actions/create-document.js" \
+    "$PACKAGE_HOME/database-actions/create-document.js" \
     -a description 'Create document in database' \
     -a parameters '[ {"name":"dbname", "required":true}, {"name":"doc", "required":true, "description": "The JSON document to insert"}, {"name":"params", "required":false} ]' \
 
 $WSK_CLI -i --apihost "$APIHOST" action update --kind "$ACTION_RUNTIME_VERSION" --auth "$AUTH" cloudant/read \
-    "$PACKAGE_HOME/packages/database-actions/read-document.js" \
+    "$PACKAGE_HOME/database-actions/read-document.js" \
     -a description 'Read document from database' \
     -a parameters '[ {"name":"dbname", "required":true}, {"name":"id", "required":true, "description": "The Cloudant document id to fetch"}, {"name":"params", "required":false}]' \
     -p id ''
 
 $WSK_CLI -i --apihost "$APIHOST" action update --kind "$ACTION_RUNTIME_VERSION" --auth "$AUTH" cloudant/read-document \
-    "$PACKAGE_HOME/packages/database-actions/read-document.js" \
+    "$PACKAGE_HOME/database-actions/read-document.js" \
     -a description 'Read document from database' \
     -a parameters '[ {"name":"dbname", "required":true}, {"name":"docid", "required":true, "description": "The Cloudant document id to fetch"}, {"name":"params", "required":false}]' \
     -p docid ''
 
 $WSK_CLI -i --apihost "$APIHOST" action update --kind "$ACTION_RUNTIME_VERSION" --auth "$AUTH" cloudant/write \
-    "$PACKAGE_HOME/packages/database-actions/write-document.js" \
+    "$PACKAGE_HOME/database-actions/write-document.js" \
     -a description 'Write document in database' \
     -a parameters '[ {"name":"dbname", "required":true}, {"name":"doc", "required":true} ]' \
     -p doc '{}'
 
 $WSK_CLI -i --apihost "$APIHOST" action update --kind "$ACTION_RUNTIME_VERSION" --auth "$AUTH" cloudant/update-document \
-    "$PACKAGE_HOME/packages/database-actions/update-document.js" \
+    "$PACKAGE_HOME/database-actions/update-document.js" \
     -a description 'Update document in database' \
     -a parameters '[ {"name":"dbname", "required":true}, {"name":"doc", "required":true}, {"name":"params", "required":false} ]' \
     -p doc '{}'
 
 $WSK_CLI -i --apihost "$APIHOST" action update --kind "$ACTION_RUNTIME_VERSION" --auth "$AUTH" cloudant/delete-document \
-    "$PACKAGE_HOME/packages/database-actions/delete-document.js" \
+    "$PACKAGE_HOME/database-actions/delete-document.js" \
     -a description 'Delete document from database' \
     -a parameters '[ {"name":"dbname", "required":true}, {"name":"docid", "required":true, "description": "The Cloudant document id to delete"},  {"name":"docrev", "required":true, "description": "The document revision number"} ]' \
     -p docid '' \
     -p docrev ''
 
 $WSK_CLI -i --apihost "$APIHOST" action update --kind "$ACTION_RUNTIME_VERSION" --auth "$AUTH" cloudant/list-documents \
-    "$PACKAGE_HOME/packages/database-actions/list-documents.js" \
+    "$PACKAGE_HOME/database-actions/list-documents.js" \
     -a description 'List all docs from database' \
     -a parameters '[ {"name":"dbname", "required":true}, {"name":"params", "required":false} ]'
 
 $WSK_CLI -i --apihost "$APIHOST" action update --kind "$ACTION_RUNTIME_VERSION" --auth "$AUTH" cloudant/list-design-documents \
-    "$PACKAGE_HOME/packages/database-actions/list-design-documents.js" \
+    "$PACKAGE_HOME/database-actions/list-design-documents.js" \
     -a description 'List design documents from database' \
     -a parameters '[ {"name":"dbname", "required":true}, {"name":"includedocs", "required":false} ]' \
 
 $WSK_CLI -i --apihost "$APIHOST" action update --kind "$ACTION_RUNTIME_VERSION" --auth "$AUTH" cloudant/create-query-index \
-    "$PACKAGE_HOME/packages/database-actions/create-query-index.js" \
+    "$PACKAGE_HOME/database-actions/create-query-index.js" \
     -a description 'Create a Cloudant Query index into database' \
     -a parameters '[ {"name":"dbname", "required":true}, {"name":"index", "required":true} ]' \
     -p index ''
 
 $WSK_CLI -i --apihost "$APIHOST" action update --kind "$ACTION_RUNTIME_VERSION" --auth "$AUTH" cloudant/list-query-indexes \
-    "$PACKAGE_HOME/packages/database-actions/list-query-indexes.js" \
+    "$PACKAGE_HOME/database-actions/list-query-indexes.js" \
     -a description 'List Cloudant Query indexes from database' \
     -a parameters '[ {"name":"dbname", "required":true} ]' \
 
 $WSK_CLI -i --apihost "$APIHOST" action update --kind "$ACTION_RUNTIME_VERSION" --auth "$AUTH" cloudant/exec-query-find \
-    "$PACKAGE_HOME/packages/database-actions/exec-query-find.js" \
+    "$PACKAGE_HOME/database-actions/exec-query-find.js" \
     -a description 'Execute query against Cloudant Query index' \
     -a parameters '[ {"name":"dbname", "required":true}, {"name":"query", "required":true} ]' \
     -p query ''
 
 $WSK_CLI -i --apihost "$APIHOST" action update --kind "$ACTION_RUNTIME_VERSION" --auth "$AUTH" cloudant/exec-query-search \
-    "$PACKAGE_HOME/packages/database-actions/exec-query-search.js" \
+    "$PACKAGE_HOME/database-actions/exec-query-search.js" \
     -a description 'Execute query against Cloudant search' \
     -a parameters '[ {"name":"dbname", "required":true}, {"name":"docid", "required":true}, {"name":"indexname", "required":true}, {"name":"search", "required":true} ]' \
     -p docid '' \
@@ -141,39 +141,39 @@ $WSK_CLI -i --apihost "$APIHOST" action update --kind "$ACTION_RUNTIME_VERSION" 
     -p search ''
 
 $WSK_CLI -i --apihost "$APIHOST" action update --kind "$ACTION_RUNTIME_VERSION" --auth "$AUTH" cloudant/exec-query-view \
-    "$PACKAGE_HOME/packages/database-actions/exec-query-view.js" \
+    "$PACKAGE_HOME/database-actions/exec-query-view.js" \
     -a description 'Call view in design document from database' \
     -a parameters '[ {"name":"dbname", "required":true}, {"name":"docid", "required":true}, {"name":"viewname", "required":true}, {"name":"params", "required":false} ]' \
     -p docid '' \
     -p viewname ''
 
 $WSK_CLI -i --apihost "$APIHOST" action update --kind "$ACTION_RUNTIME_VERSION" --auth "$AUTH" cloudant/manage-bulk-documents \
-    "$PACKAGE_HOME/packages/database-actions/manage-bulk-documents.js" \
+    "$PACKAGE_HOME/database-actions/manage-bulk-documents.js" \
     -a description 'Create, Update, and Delete documents in bulk' \
     -a parameters '[ {"name":"dbname", "required":true}, {"name":"docs", "required":true}, {"name":"params", "required":false} ]' \
     -p docs '{}'
 
 $WSK_CLI -i --apihost "$APIHOST" action update --kind "$ACTION_RUNTIME_VERSION" --auth "$AUTH" cloudant/delete-view \
-    "$PACKAGE_HOME/packages/database-actions/delete-view.js" \
+    "$PACKAGE_HOME/database-actions/delete-view.js" \
     -a description 'Delete view from design document' \
     -a parameters '[ {"name":"dbname", "required":true}, {"name":"docid", "required":true}, {"name":"viewname", "required":true}, {"name":"params", "required":false} ]' \
     -p docid '' \
     -p viewname ''
 
 $WSK_CLI -i --apihost "$APIHOST" action update --kind "$ACTION_RUNTIME_VERSION" --auth "$AUTH" cloudant/delete-query-index \
-    "$PACKAGE_HOME/packages/database-actions/delete-query-index.js" \
+    "$PACKAGE_HOME/database-actions/delete-query-index.js" \
     -a description 'Delete index from design document' \
     -a parameters '[ {"name":"dbname", "required":true}, {"name":"docid", "required":true}, {"name":"indexname", "required":true}, {"name":"params", "required":false} ]' \
     -p docid '' \
     -p indexname ''
 
 $WSK_CLI -i --apihost "$APIHOST" action update --kind "$ACTION_RUNTIME_VERSION" --auth "$AUTH" cloudant/read-changes-feed \
-    "$PACKAGE_HOME/packages/database-actions/read-changes-feed.js" \
+    "$PACKAGE_HOME/database-actions/read-changes-feed.js" \
     -a description 'Read Cloudant database changes feed (non-continuous)' \
     -a parameters '[ {"name":"dbname", "required":true}, {"name":"params", "required":false} ]'
 
 $WSK_CLI -i --apihost "$APIHOST" action update --kind "$ACTION_RUNTIME_VERSION" --auth "$AUTH" cloudant/create-attachment \
-    "$PACKAGE_HOME/packages/database-actions/create-update-attachment.js" \
+    "$PACKAGE_HOME/database-actions/create-update-attachment.js" \
     -a description 'Create document attachment in database' \
     -a parameters '[ {"name":"dbname", "required":true}, {"name":"docid", "required":true}, {"name":"docrev", "required":true}, {"name":"attachment", "required":true}, {"name":"attachmentname", "required":true}, {"name":"contenttype", "required":true}, {"name":"params", "required":false} ]' \
     -p docid '' \
@@ -183,14 +183,14 @@ $WSK_CLI -i --apihost "$APIHOST" action update --kind "$ACTION_RUNTIME_VERSION" 
     -p contenttype ''
 
 $WSK_CLI -i --apihost "$APIHOST" action update --kind "$ACTION_RUNTIME_VERSION" --auth "$AUTH" cloudant/read-attachment \
-    "$PACKAGE_HOME/packages/database-actions/read-attachment.js" \
+    "$PACKAGE_HOME/database-actions/read-attachment.js" \
     -a description 'Read document attachment from database' \
     -a parameters '[ {"name":"dbname", "required":true}, {"name":"docid", "required":true}, {"name":"attachmentname", "required":true}, {"name":"params", "required":false} ]' \
     -p docid '' \
     -p attachmentname ''
 
 $WSK_CLI -i --apihost "$APIHOST" action update --kind "$ACTION_RUNTIME_VERSION" --auth "$AUTH" cloudant/update-attachment \
-    "$PACKAGE_HOME/packages/database-actions/create-update-attachment.js" \
+    "$PACKAGE_HOME/database-actions/create-update-attachment.js" \
     -a description 'Update document attachment in database' \
     -a parameters '[ {"name":"dbname", "required":true}, {"name":"docid", "required":true}, {"name":"docrev", "required":true}, {"name":"attachment", "required":true}, {"name":"attachmentname", "required":true}, {"name":"contenttype", "required":true}, {"name":"params", "required":false} ]' \
     -p docid '' \
@@ -200,7 +200,7 @@ $WSK_CLI -i --apihost "$APIHOST" action update --kind "$ACTION_RUNTIME_VERSION" 
     -p contenttype ''
 
 $WSK_CLI -i --apihost "$APIHOST" action update --kind "$ACTION_RUNTIME_VERSION" --auth "$AUTH" cloudant/delete-attachment \
-    "$PACKAGE_HOME/packages/database-actions/delete-attachment.js" \
+    "$PACKAGE_HOME/database-actions/delete-attachment.js" \
     -a description 'Delete document attachment from database' \
     -a parameters '[ {"name":"dbname", "required":true}, {"name":"docid", "required":true}, {"name":"docrev", "required":true}, {"name":"attachmentname", "required":true}, {"name":"params", "required":false} ]' \
     -p docid '' \
